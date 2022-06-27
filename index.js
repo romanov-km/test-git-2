1d18ee4087e69042098e8c95b66fbf334730ed74
Kirill Romanov <workrkm@mail.ru>
2022-06-27 21:27:32 +0300

    add css for html styles

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ console.log('Hello World');
 console.error('Error');
 
 console.log('Hello');
+
+const createH1 = document.createElement('h1');
+createH1.textContent = 'Первый сайт за 50 рублей';
+document.body.append(createH1);
